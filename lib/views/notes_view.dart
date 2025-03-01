b4d82108ecfa55641d7e03591c9cb96e98df6caf
Mohand0sman <mohand.osman.adam@gmail.com>
2025-03-01 20:34:55 +0200

    display data

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,33 +1,35 @@
 import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
-// import '../widgets/notes_view_body.dart';
-
 class NotesView extends StatelessWidget {
-  const NotesView({super.key});
+  const NotesView({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Colors.transparent,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            context: context,
-            builder: (context) {
-              return const AddNoteBottomSheet();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                context: context,
+                builder: (context) {
+                  return const AddNoteBottomSheet();
+                });
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/views/widgets/notes_list_veiw.dart';
 
-class NotesViewBody extends StatelessWidget {
-  const NotesViewBody({super.key});
+import '../../cubits/notes_cubit/notes_cubit.dart';
+import 'custom_app_bar.dart';
+
+class NotesViewBody extends StatefulWidget {
+  const NotesViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 0),
+      padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
           SizedBox(
             height: 50,
           ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: CustomAppBar(
-              title: "Notes",
-              icon: Icons.search,
-            ),
+          CustomAppBar(
+            title: 'Notes',
+            icon: Icons.search,
           ),
           Expanded(
             child: NotesListView(),
-          )
+          ),
         ],
       ),
     );
